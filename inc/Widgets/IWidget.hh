45b67d4ef0d6e6668a4330fc9eb641e901a61dfb
unreformable1 <danieltroszczynski@wp.pl>
2022-09-14 00:00:38 +0200

    Updated interface for widgets

--- inc/Widgets/IWidget.hh
+++ inc/Widgets/IWidget.hh
@@ -10,9 +10,4 @@ public:
     virtual ~IWidget() = default;
     virtual bool isMouseOver(const sf::RenderWindow& render_window) = 0;
     virtual void draw(sf::RenderTarget& render_target) = 0;
-
-    virtual void setFont(const sf::Font& font) = 0;
-    virtual void setTextSize(unsigned int size) = 0;
-    virtual void setTextColor(const sf::Color& color) = 0;
-    virtual void setBgColor(const sf::Color& color) = 0;
 };
