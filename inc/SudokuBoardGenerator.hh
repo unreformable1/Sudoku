f413185443bee1a3a89b57f419a21243067892ba
unreformable1 <danieltroszczynski@wp.pl>
2022-09-18 00:19:17 +0200

    Added the rest of algorithm for generating random playable sudoku boardgit add inc/SudokuBoardGenerator.hh ! Methods are now static.

--- inc/SudokuBoardGenerator.hh
+++ inc/SudokuBoardGenerator.hh
@@ -1,30 +1,87 @@
 #pragma once
 
-#include "SudokuBoard.hh"
 #include "SudokuBoardSolver.hh"
-
 
 
 
 class SudokuBoardGenerator
 {
 public:
-    void generate(SudokuBoard& output_board);
+    static void generate(SudokuBoard& board);
 
 
 private:
-    void solve(SudokuBoard& board);
-
-
-private:
-    SudokuBoardSolver m_solver;
+    static void generateFull(SudokuBoard& board);
 };
 
-void SudokuBoardGenerator::generate(SudokuBoard& output_board)
+void SudokuBoardGenerator::generate(SudokuBoard& board)
 {
-    SudokuBoard board;
+    // First phase - generate full board and make copy of it
+    board.clear();
+    generateFull(board);
 
-    m_solver.solve(board);
+    SudokuBoard full_board(board);
 
-    output_board = board;
+    // Second phase - keep removing cells to the moment when there is barely 1 solution
+    {
+        int index;
+        while(SudokuBoardSolver::solutions(board, 2) == 1)
+        {
+            index = RNG::get(0, 80);
+            while(board(index) == 0)
+            {
+                if(++index == 81)
+                    index = 0;
+            }
+            board.remove(index);
+        }
+        board.set(index, full_board(index));
+    }
+    // Third phase - remove as much as possible cells so there is still 1 unique solution
+    {
+        for(int i = 0; i < 81; ++i)
+        {
+            if(board(i) == 0)
+                continue;
+            
+            board.remove(i);
+
+            if(SudokuBoardSolver::solutions(board, 2) > 1)
+            {
+                board.set(i, full_board(i));
+            }
+        }
+    }
+}
+
+void SudokuBoardGenerator::generateFull(SudokuBoard& board)
+{
+    int pos = 0;
+    int combinations_count[board.size()];
+    for(int i = 0; i < board.size(); ++i)
+        combinations_count[i] = 0;
+
+    while(!board.full())
+    {
+        board.set(pos, RNG::get(1, 9));
+
+        while(!board.valid(pos))
+        {
+            combinations_count[pos]++;
+
+            if(combinations_count[pos] == 9)    // out of number combinations for current cell
+            {
+                // Reset actual cell and return to previous cell
+                board.set(pos, 0);
+                combinations_count[pos] = 0;
+                pos--;
+            }
+
+            board.set(pos, board.get(pos) + 1);
+            if(board.get(pos) > 9)
+                board.set(pos, 1);
+        }
+
+        pos++;
+    }
 }
