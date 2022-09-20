2aa64f5caad5de6d3ef3570a205ad0f3fa04a171
unreformable1 <danieltroszczynski@wp.pl>
2022-09-20 19:06:07 +0200

    Moved updating view from SudokuBoardView to SudokuBoardController (so it controlls when to update View). Minor changes

--- inc/SudokuBoardController.hh
+++ inc/SudokuBoardController.hh
@@ -2,6 +2,7 @@
 
 #include "SudokuBoard.hh"
 #include "SudokuBoardView.hh"
+#include "SudokuBoardGenerator.hh"
 
 #include <chrono>
 
@@ -11,11 +12,15 @@ class SudokuBoardController
 public:
     SudokuBoardController(sf::RenderWindow& render_window, SudokuBoard& board, SudokuBoardView& board_view);
     void onKey(sf::Keyboard::Key key);
-    void onMouseClick(sf::Mouse::Button button);
+    void onMouseClick(sf::Mouse::Button button, const sf::Vector2i& mouse_pos);
     void onScreenNum(int num);
     void onDeleteButton();
     void onUndoButton();
     void onHintButton();
+
+
+private:
+    void updateView();
 
 
 private:
@@ -27,39 +32,41 @@ private:
 SudokuBoardController::SudokuBoardController(sf::RenderWindow& render_window, SudokuBoard& board, SudokuBoardView& board_view)
 :   m_renderWindow(render_window), m_board(board), m_boardView(board_view)
 {
-
+    updateView();
 }
 
 void SudokuBoardController::onKey(sf::Keyboard::Key key)
 {
     if(key >= sf::Keyboard::Num1 && key <= sf::Keyboard::Num9)
     {
-        std::vector<CellView>& cells = m_boardView.getCells();
+        std::vector<Widget*>& cells = m_boardView.getChildren();
         for(int i = 0; i < cells.size(); ++i)
         {
-            if(cells[i].hasFocus())
+            if(cells[i]->getFocus())
             {
                 m_board.set(i, key - 26);   // "key - 26" changes sf::Keyboard::Key enum value to (1-9) digit
             }
         }
+        updateView();
         m_boardView.draw(m_renderWindow);
         m_renderWindow.display();
     }
     else if(key == sf::Keyboard::C)
     {
         m_board.clear();
+        updateView();
         m_boardView.draw(m_renderWindow);
         m_renderWindow.display();
     }
     else if(key == sf::Keyboard::N)
     {
-        std::cout << std::endl;
         if(!m_board.valid())
         {
-            std::cout << "Cannot find solutions to a sudoku board that is not correct.";
+            std::cout << "Cannot find solutions to a sudoku board that is not correct." << std::endl;
             return;
         }
 
+        std::cout << std::endl;
         auto start = std::chrono::high_resolution_clock::now();
         const int& solutions_count = SudokuBoardSolver::solutions(m_board);
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start);
@@ -77,12 +84,19 @@ void SudokuBoardController::onKey(sf::Keyboard::Key key)
     }
     else if(key == sf::Keyboard::S)
     {
+        if(!m_board.valid())
+        {
+            std::cout << std::endl << "Can't solve invalid board" << std::endl;
+            return;
+        }
+
         std::cout << std::endl;
         auto start = std::chrono::high_resolution_clock::now();
         SudokuBoardSolver::solve(m_board);
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start);
         std::cout << "Board solving time: " << duration.count() << " µs" << std::endl;
 
+        updateView();
         m_boardView.draw(m_renderWindow);
         m_renderWindow.display();
     }
@@ -94,41 +108,46 @@ void SudokuBoardController::onKey(sf::Keyboard::Key key)
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start);
         std::cout << "Board generation time: " << duration.count() << " µs" << std::endl;
 
+        updateView();
         m_boardView.draw(m_renderWindow);
         m_renderWindow.display();
     }
 }
 
-void SudokuBoardController::onMouseClick(sf::Mouse::Button button)
+void SudokuBoardController::onMouseClick(sf::Mouse::Button button, const sf::Vector2i& mouse_pos)
 {
     if(button == sf::Mouse::Left)
     {
-        std::vector<CellView>& cells = m_boardView.getCells();
-        for(CellView& cell : cells)
+        std::vector<Widget*>& cells = m_boardView.getChildren();
+        for(Widget* cell : cells)
         {
-            if(cell.isMouseOver(m_renderWindow))
+            if(cell->contains(mouse_pos))
             {
-                cell.setFocus(true);
+                cell->setFocus(true);
             }
             else
-                cell.setFocus(false);
+            {
+                cell->setFocus(false);
+            }
         }
+        updateView();
         m_boardView.draw(m_renderWindow);
         m_renderWindow.display();
     }
     else if(button == sf::Mouse::Right)
     {
-        std::vector<CellView>& cells = m_boardView.getCells();
+        std::vector<Widget*>& cells = m_boardView.getChildren();
         for(int i = 0; i < cells.size(); ++i)
         {
-            if(cells[i].isMouseOver(m_renderWindow))
+            if(cells[i]->contains(mouse_pos))
             {
-                cells[i].setFocus(true);
+                cells[i]->setFocus(true);
                 m_board.set(i, 0);
             }
             else
-                cells[i].setFocus(false);
+                cells[i]->setFocus(false);
         }
+        updateView();
         m_boardView.draw(m_renderWindow);
         m_renderWindow.display();
     }
@@ -136,28 +155,30 @@ void SudokuBoardController::onMouseClick(sf::Mouse::Button button)
 
 void SudokuBoardController::onScreenNum(int num)
 {
-    std::vector<CellView>& cells = m_boardView.getCells();
+    std::vector<Widget*>& cells = m_boardView.getChildren();
     for(int i = 0; i < 81; ++i)
     {
-        if(cells[i].hasFocus())
+        if(cells[i]->getFocus())
         {
             m_board.set(i, num);
         }
     }
+    updateView();
     m_boardView.draw(m_renderWindow);
     m_renderWindow.display();
 }
 
 void SudokuBoardController::onDeleteButton()
 {
-    std::vector<CellView>& cells = m_boardView.getCells();
+    std::vector<Widget*>& cells = m_boardView.getChildren();
     for(int i = 0; i < cells.size(); ++i)
     {
-        if(cells[i].hasFocus())
+        if(cells[i]->getFocus())
         {
             m_board.set(i, 0);
         }
     }
+    updateView();
     m_boardView.draw(m_renderWindow);
     m_renderWindow.display();
 }
@@ -170,4 +191,26 @@ void SudokuBoardController::onUndoButton()
 void SudokuBoardController::onHintButton()
 {
 
+}
+
+void SudokuBoardController::updateView()
+{
+    for(int i = 0; i < 81; ++i)
+    {
+        Widget* cell = m_boardView.getChild(i);
+
+        // Update view of each cell number
+        if(m_board.get(i) == 0)
+            cell->setTextString("");
+        else
+            cell->setTextString(std::to_string(m_board.get(i)));
+
+        // Update view of each cell background
+        if(cell->getFocus())
+            cell->setBgColor(sf::Color(137, 207, 240));    // baby blue :P
+        else if(!m_board.valid(i))
+            cell->setBgColor(sf::Color(255, 119, 121));    // baby red :P
+        else
+            cell->setBgColor(sf::Color::White);
+    }
 }
